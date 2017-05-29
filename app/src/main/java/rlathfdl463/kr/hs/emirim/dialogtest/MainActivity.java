2c559d57176237257efd443543870ea661f12d7a
Da in <yycc0524@naver.com>
2017-05-29 19:15:38 +0900

    #5 checkBox dialog

--- app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
@@ -25,13 +25,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("첫번째 다이얼로그");
         dialog.setIcon(R.mipmap.ic_launcher);
 //        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
-        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+//단순목록
+/*        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 butDialog.setText(itemArr[i]);
             }
+        });*/
+//라디오 버튼 목록
+/*        dialog.setSingleChoiceItems(itemArr, 0, new DialogInterface.OnClickListener(){
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            butDialog.setText(itemArr[i]);
+        }
+    });*/
+//      체크 박스 목록
+//      선택 여부 배열
+        boolean[] checkedItems={true, false, true, false};
+        dialog.setMultiChoiceItems(itemArr, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i, boolean b) {
+                if(b)
+                    butDialog.setText(itemArr[i]);
+            }
         });
-        dialog.setPositiveButton("OK", null);
+
+//        dialog.setPositiveButton("OK", null);
         dialog.show();
     }
 }
